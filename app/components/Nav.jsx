3dfabbed004497c385188297e4ced7c233ca6fe9
adsznzhang <adszn@hotmail.com>
2017-05-03 19:36:57 +0800

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+
+var Nav = React.createClass({
+    render: function () {
+        return (
+                <div className="top-bar">
+                <div className="top-bar-left">
+                <ul className="menu">
+                <li className="menu-text">Timer App</li>
+                <li>
+                <IndexLink to="/" activeClassName="active-link">Set Time</IndexLink>
+                </li>
+                <li>
+                <IndexLink to="/" activeClassName="active-link">Count Down</IndexLink>
+                </li>
+                </ul>
+                </div>
+                <div className="top-bar-right">
+                <ul className="menu">
+                <li>App created by <a href="#" target="_blank">zhangzhen</a></li>
+                </ul>
+                </div>
+                </div>
+        )
+    }
+})
+
+module.exports = Nav;
---
+++ readme.md
@@ -0,0 +1,1 @@
+Boilplaite React App
